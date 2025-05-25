9c45dddfb218d912227db95188e8be6f30c6fa8c
Kamelmostafa21 <kamelmo284@gmail.com>
2025-05-25 23:04:42 +0300

    python ci-cd(script)

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,10 +1,10 @@
 node('agent-1') {
     
     stage('Build Docker image') {
-        sh "docker build -t kamelmostafa/python-ci-cd(script):v${env.BUILD_NUMBER} ."
+        sh "docker build -t kamelmostafa/python-ci-cd-script:v${env.BUILD_NUMBER} ."
     }
 
     stage('Push Docker image') {
-        sh "docker push kamelmostafa/python-ci-cd(script):v${env.BUILD_NUMBER}"
+        sh "docker push kamelmostafa/python-ci-cd-script:v${env.BUILD_NUMBER}"
     }
 }
